bb8bc471a999b4098143b92a7012b9be4746c204
devlopersumit <sumitsj2002@gmail.com>
2025-11-10 20:20:50 +0530

    Added Testimonials section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AITools from '../components/AITools'
+import Testimonials from '../components/Testimonials'
 
 const Home = () => {
   return (
     <>
     <Navbar /> 
     <Hero />
-    <AITools />   
+    <AITools />
+    <Testimonials />   
     </>
   )
 }
